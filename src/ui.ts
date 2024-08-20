19196e1c51f80295bd627f90e7e8db479bf8872e
haile <haileleulfiseha@gmail.com>
2024-08-20 22:30:47 +0300

    some UI adjustment

--- src/ui.ts
+++ src/ui.ts
@@ -2,7 +2,7 @@ import { RENDERER, TEXT, TEXTURE } from '../types';
 export class UI {
 	renderer: RENDERER;
 	boardTexture: TEXTURE
-	move: number = 0
+	move: number = 0	
 	level: number = 1
 	score: number = 0
 	moveText: TEXT
@@ -29,28 +29,39 @@ export class UI {
 		this.renderer.stage(boardbg, text);
 	}
 	createLevelBoard(gridPos: { x: number, y: number }) {
+		// const boardbg = this.renderer.createSprite(this.boardTexture);
+		// const margin = 50
+		// boardbg.position.set(margin, gridPos.y+margin);
+		// boardbg.width = gridPos.x - 2*margin
+		// boardbg.height = 100;
+		// const text = this.renderer.write(`Level : ${this.level}`, boardbg.position.x + boardbg.width / 2 - 40, boardbg.position.y + boardbg.height / 2)
+		// this.levelText = text
+		// text.zIndex = 1
+
+
 		const boardbg = this.renderer.createSprite(this.boardTexture);
 		const margin = 50
 		boardbg.position.set(margin, gridPos.y+margin);
 		boardbg.width = gridPos.x - 2*margin
 		boardbg.height = 100;
-		const text = this.renderer.write(`Level : ${this.level}`, boardbg.position.x + boardbg.width / 2 - 40, boardbg.position.y + boardbg.height / 2)
-		this.levelText = text
+		const text = this.renderer.write(`Score: ${this.score}`, boardbg.position.x + boardbg.width / 2 - 40, boardbg.position.y + boardbg.height / 2)
+		this.scoreText = text
 		text.zIndex = 1
 
-		 // Score display background
-		 const scoreBoardbg = this.renderer.createSprite(this.boardTexture);
-		 scoreBoardbg.position.set(margin, boardbg.position.y + boardbg.height + margin);
-		 scoreBoardbg.width = boardbg.width;
-		 scoreBoardbg.height = 100; 
-		 this.scoreBoard = scoreBoardbg;
+		//  // Score display background
+		//  const scoreBoardbg = this.renderer.createSprite(this.boardTexture);
+		//  scoreBoardbg.position.set(margin, boardbg.position.y + boardbg.height + margin);
+		//  scoreBoardbg.width = boardbg.width;
+		//  scoreBoardbg.height = 100; 
+		//  this.scoreBoard = scoreBoardbg;
  
-		 // Score display text
-		 const scoreText = this.renderer.write(`Score : ${this.score}`, scoreBoardbg.position.x + scoreBoardbg.width / 2 - 40, scoreBoardbg.position.y + scoreBoardbg.height / 2);
-		 this.scoreText = scoreText;
-		 scoreText.zIndex = 1;
+		//  // Score display text
+		//  const scoreText = this.renderer.write(`Score : ${this.score}`, scoreBoardbg.position.x + scoreBoardbg.width / 2 - 40, scoreBoardbg.position.y + scoreBoardbg.height / 2);
+		//  this.scoreText = scoreText;
+		//  scoreText.zIndex = 1;
  
-		this.renderer.stage(boardbg, text, scoreBoardbg, scoreText);
+		this.renderer.stage(boardbg, text);
+		// this.renderer.stage(boardbg, text, scoreBoardbg, scoreText);
 	}
 	updateMove(move: number) {
 		this.move = move
